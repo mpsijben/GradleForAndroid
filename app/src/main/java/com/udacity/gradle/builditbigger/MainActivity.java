e8af43f5bc59e583f09ccb31e462feb1c41c3054
Menno Sijben <mennosijben@gmail.com>
2015-12-09 22:03:01 +0100

    feat: Create a Java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,19 +1,24 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.Joker;
+
 
 public class MainActivity extends ActionBarActivity {
+    private Joker joker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joker = new Joker();
     }
 
 
@@ -40,7 +45,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, joker.GetJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ javajokes/src/main/java/com/example/Joker.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Joker {
+
+    public Joker()
+    {
+
+    }
+
+    public String GetJoke()
+    {
+        return "Two fish are in a tank. One turns to the other and asks “How do you drive this thing?”";
+    }
+}
